9c9a14cad55eb1fe718c4b4589c5bb5257c2f2b6
shakhawath <m.shakhawath18@gmail.com>
2022-05-26 09:16:22 +0600

    added userCollection api

---
+++ README.md
@@ -0,0 +1,32 @@
+# Car Zone
+
+## [Live Website Link](https://car-zone-e9458.web.app/)
+
+Private client-side repository link: [ https://github.com/ProgrammingHeroWC4/warehouse-management-client-side-shakhawath1]( https://github.com/ProgrammingHeroWC4/warehouse-management-client-side-shakhawath1)
+
+Private server-side repository link: [https://github.com/shakhawath1/car-zone-server-side](https://github.com/shakhawath1/car-zone-server-side)
+
+## Description
+This is an website of an car warehouse.
+In this project, there are three main pages, one Error-404 page:
+
+### `HOME page`
+
+Consists of Banner, Intro, Items, JoinUs
+
+### `Blogs`
+
+4 questions and answer
+
+### `About`
+
+About the warehouse
+
+### `ERROR-404 page`
+
+If page not found.
+
+### `All pages shared header & footer`
+
+### `There are some private pages and routes which requires user to log in`
+
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,18 +21,39 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        const productsCollecton = client.db("ac-dc").collection("products");
+        const productCollecton = client.db("ac-dc").collection("products");
+        const orderCollecton = client.db("ac-dc").collection("orders");
+        const userCollecton = client.db("ac-dc").collection("users");
 
         // all products API
         app.get('/product', async (req, res) => {
             const query = {};
-            const cursor = productsCollecton.find(query);
+            const cursor = productCollecton.find(query);
             const products = await cursor.toArray();
             res.send(products);
+        });
+
+        // get one product
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollecton.findOne(query);
+            res.send(product);
+        });
+
+
+        // put users
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollecton.upsertOne(filter, updateDoc, options);
+            res.send(result);
         })
-
-
-
 
 
 
