10d2864213c7e8fc98b166c5049cd6345acc6adc
shakhawath <m.shakhawath18@gmail.com>
2022-05-26 14:53:06 +0600

    added post-order api

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ut3bs.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -23,7 +20,7 @@ async function run() {
     try {
         await client.connect();
         const productCollection = client.db("ac-dc").collection("products");
-        // const orderCollection = client.db("ac-dc").collection("orders");
+        const orderCollection = client.db("ac-dc").collection("orders");
         const userCollection = client.db("ac-dc").collection("users");
 
         // all products API
@@ -41,6 +38,14 @@ async function run() {
             const product = await productCollection.findOne(query);
             res.send(product);
         });
+
+        // add prder
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            return res.send({ success: true, result });
+        });
+
 
 
         // put users
